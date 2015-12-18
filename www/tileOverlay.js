fe19662bfd5e30c9dc0f62d51c276d74943e9fcc
Brian Flood <bFlood@spatialdatalogic.com>
2015-12-18 14:23:21 -0500

    Create tileOverlay.js

---
+++ www/tileOverlay.js
@@ -0,0 +1,9 @@
+tabris.registerWidget("_ESTileOverlay", {
+  _type: "tabris.maps.tileoverlay",
+  _properties: {
+    tileUrlFormat: {type: "string", nocache: true},
+    visible: {type: "boolean", nocache: true},
+    opacity: {type: "number", nocache: true},
+    tileSize: {type: "number", nocache: true}
+  }
+});
