472f464a478b9878a673cbaa3bcb53754a4c2b25
Brian Flood <bFlood@spatialdatalogic.com>
2015-12-18 12:26:39 -0500

    Update markerWidget.js

--- www/markerWidget.js
+++ www/markerWidget.js
@@ -1,7 +1,9 @@
 tabris.registerWidget("_ESMarker", {
   _type: "tabris.maps.marker",
   _properties: {
-    latLng: {type: "array", nocache: true}
+    latLng: {type: "array", nocache: true},
+    image: {type: "image", nocache: true},
+    color: {type: "color", nocache: true}
   },
   _events: {tap: true}
 });
