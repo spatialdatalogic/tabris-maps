1a8e1b6e3c3e2dfc1b58d4870f7ceefb607030e2
Brian Flood <bFlood@spatialdatalogic.com>
2015-12-22 08:40:31 -0500

    Update TileOverlayPropertyHandler.java

--- src/android/com/eclipsesource/tabris/maps/TileOverlayPropertyHandler.java
+++ src/android/com/eclipsesource/tabris/maps/TileOverlayPropertyHandler.java
@@ -7,7 +7,7 @@ import android.graphics.Color;
 
 import com.eclipsesource.tabris.android.TabrisActivity;
 import com.eclipsesource.tabris.android.TabrisContext;
-import com.eclipsesource.tabris.android.internal.toolkit.property.IPropertyHandler;
+import com.eclipsesource.tabris.android.TabrisPropertyHandler;
 import com.eclipsesource.tabris.client.core.model.Properties;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.TileOverlay;
@@ -17,7 +17,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class TileOverlayPropertyHandler<T extends TileOverlay> implements IPropertyHandler<T> {
+public class TileOverlayPropertyHandler<T extends TileOverlay> implements TabrisPropertyHandler<T> {
 
   private final Activity activity;
   private final TabrisContext context;
@@ -34,13 +34,20 @@ public class TileOverlayPropertyHandler<T extends TileOverlay> implements IPrope
 
   @Override
   public Object get( T tileOverlay, String property ) {
-    System.out.println( "get on " + tileOverlay + ": " + property );
+      switch( property ) {
+        case "opacity":
+            return tileProvider.getOpacity();
+            
+        case "zindex":
+            return tileOverlay.getZIndex();
+        case "visible":
+            return tileOverlay.isVisible();
+      }
     return null;
   }
 
   @Override
   public void set( T tileOverlay, Properties properties ) {
-      System.out.println( "set: " + properties );
       for( String key : properties.getAll().keySet() ) {
         setProperty( key, tileOverlay, properties );
       }
@@ -48,8 +55,6 @@ public class TileOverlayPropertyHandler<T extends TileOverlay> implements IPrope
   
   private void setProperty( String key, T tileOverlay, Properties properties ) {
       switch( key ) {
-
-            
         case "opacity":
             tileProvider.setOpacity(properties.getFloat( "opacity" ));
             break;
@@ -61,8 +66,6 @@ public class TileOverlayPropertyHandler<T extends TileOverlay> implements IPrope
         case "visible":
             tileOverlay.setVisible(properties.getBoolean( "visible" ));
             break;  
-            
-          
       }
   }  
   
