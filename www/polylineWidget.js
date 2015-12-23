9b0562b28ede480e9bc295d5fd439f23fd2bc6f0
Brian Flood <bFlood@spatialdatalogic.com>
2015-12-23 09:35:52 -0500

    Update polylineWidget.js

--- www/polylineWidget.js
+++ www/polylineWidget.js
@@ -2,16 +2,12 @@ tabris.registerWidget("_ESPolyline", {
   _type: "tabris.maps.polyline",
   _properties: {
     visible: {type: "boolean", nocache: true},
+    geometry: {type: "any", nocache: true},
     clickable: {type: "boolean", nocache: true},
     geodesic: {type: "boolean", nocache: true},
     color: {type: "color", nocache: true},
     width: {type: "number", nocache: true},
     zindex: {type: "number", nocache: true},
   },
-  _events: {tap: true},
-  setGeometry: function(geojson) {
-    this._nativeCall("setGeometry", {
-      "geojson": geojson
-    });  
-  }
+  _events: {tap: true}
 });
