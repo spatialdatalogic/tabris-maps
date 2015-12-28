6b21c9fa89a4d781ff3962fb3d1cb3546b5f280b
Brian Flood <bFlood@spatialdatalogic.com>
2015-12-28 14:59:01 -0500

    Update ESMap.swift

--- src/ios/ESMap.swift
+++ src/ios/ESMap.swift
@@ -73,19 +73,7 @@ public class ESMap : BasicWidget, MKMapViewDelegate, UIGestureRecognizerDelegate
         }
     }
     
-    public var zoom: Int {
-        get {
-            return Int(log2(360 * (Double(self.frame.size.width/256) / self.region.span.longitudeDelta)) + 1);
-        }
-
-        set (newZoomLevel){
-            setCenterCoordinate(self.centerCoordinate, zoomLevel: newZoomLevel, animated: false)
-        }
-    }
-    private func setCenterCoordinate(coordinate: CLLocationCoordinate2D, zoomLevel: Int, animated: Bool){
-        let span = MKCoordinateSpanMake(0, 360 / pow(2, Double(zoomLevel)) * Double(self.frame.size.width) / 256)
-        setRegion(MKCoordinateRegionMake(centerCoordinate, span), animated: animated)
-    }    
+ 
     
     
     public var region: Array<Double> {
